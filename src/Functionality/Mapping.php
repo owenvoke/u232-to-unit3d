6eb61b94a0ebc549aa5030228bb5b9d285f5b380
pxgamer <owzie123@gmail.com>
2017-12-17 19:00:36 +0000

    Added Mapping

---
+++ src/Functionality/Mapping.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace pxgamer\U232ToUnit3d\Functionality;
+
+use Carbon\Carbon;
+
+/**
+ * Class Mapping.
+ */
+class Mapping
+{
+    /**
+     * @param string $type
+     * @param \stdClass $data
+     * @return array
+     */
+    public static function map(string $type, \stdClass $data): array
+    {
+        return self::{'map'.$type}($data);
+    }
+
+    /**
+     * @param \stdClass $data
+     * @return array
+     */
+    public static function mapUser(\stdClass $data): array
+    {
+        return [
+            'username'   => $data->username,
+            'password'   => $data->passhash ?? null,
+            'passkey'    => $data->secret ?? null,
+            'group_id'   => $data->class ?? 1,
+            'email'      => $data->email ?? null,
+            'uploaded'   => $data->uploaded ?? 0,
+            'downloaded' => $data->downloaded ?? 0,
+            'title'      => $data->title ?? null,
+            'image'      => $data->avatar ?? null,
+            'created_at' => Carbon::createFromTimestamp(strtotime($data->added)),
+        ];
+    }
+
+    /**
+     * @param \stdClass $data
+     * @return array
+     */
+    public static function mapTorrent(\stdClass $data): array
+    {
+        return [
+            'info_hash'   => $data->info_hash,
+            'name'        => $data->name ?? null,
+            'size'        => $data->size ?? 0,
+            'description' => $data->descr ?? null,
+            'seeders'     => $data->seeders ?? 0,
+            'leechers'    => $data->leechers ?? 0,
+            'num_files'   => $data->numfiles ?? 0,
+            'created_at'  => Carbon::createFromTimestamp(strtotime($data->added)),
+        ];
+    }
+}
